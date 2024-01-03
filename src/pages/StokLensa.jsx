ed4184767d0ca55e80a46438a83a3dbe308b06e0
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-01-03 20:08:36 +0700

    feat(stoklensa) : add react-data-table-component

--- src/pages/StokLensa.jsx
+++ src/pages/StokLensa.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import useDocumentTitle from "../utils/useDocumentTitle";
 import Breadcrumb from "../components/Breadcrumb";
+import DataTable from "react-data-table-component";
 
 const StokLensa = () => {
   useDocumentTitle("Stok lensa");
+  const columns = [
+    {
+      name: "Nama Lensa",
+    },
+  ];
   return (
     <div className="content-wrapper">
       <Breadcrumb title="Stok Lensa" />
@@ -13,7 +19,9 @@ const StokLensa = () => {
           <div className="row">
             <div className="col-lg-12">
               <div className="card">
-                <div className="card-body"></div>
+                <div className="card-body">
+                  <DataTable columns={columns} pagination highlightOnHover />
+                </div>
               </div>
             </div>
           </div>
