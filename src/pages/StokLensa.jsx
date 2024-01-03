bee65dca308246139926201f778a094da06c0688
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-01-03 20:15:03 +0700

    feat(stoklensa): get data stok lensa using axios

--- src/pages/StokLensa.jsx
+++ src/pages/StokLensa.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import useDocumentTitle from "../utils/useDocumentTitle";
 import Breadcrumb from "../components/Breadcrumb";
 import DataTable from "react-data-table-component";
+import axios from "axios";
 
 const StokLensa = () => {
   useDocumentTitle("Stok lensa");
+  const [data, setData] = useState([]);
   const columns = [
     {
       name: "Nama Lensa",
+      selector: (row) => row.product_name,
     },
   ];
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const URL = import.meta.env.VITE_API_URL;
+        const response = await axios.get(URL + "api/products", {
+          headers: {
+            Authorization: localStorage.getItem("user-token"),
+          },
+        });
+        setData(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className="content-wrapper">
       <Breadcrumb title="Stok Lensa" />
@@ -20,7 +43,12 @@ const StokLensa = () => {
             <div className="col-lg-12">
               <div className="card">
                 <div className="card-body">
-                  <DataTable columns={columns} pagination highlightOnHover />
+                  <DataTable
+                    columns={columns}
+                    data={data}
+                    pagination
+                    highlightOnHover
+                  />
                 </div>
               </div>
             </div>
