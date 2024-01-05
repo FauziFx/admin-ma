926986643cd55d9301ce881decf04df18b25a28f
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-01-05 15:21:09 +0700

    feta(stoklensa): add edit stock

--- src/pages/StokLensa.jsx
+++ src/pages/StokLensa.jsx
@@ -6,12 +6,25 @@ import axios from "axios";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 const StokLensa = () => {
-  useDocumentTitle("Stok lensa");
   const navigate = useNavigate();
+  const URL = import.meta.env.VITE_API_URL;
   const { id } = useParams();
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState([]);
+  const [lensa, setLensa] = useState("");
+  useDocumentTitle(
+    "Stok Lensa " +
+      lensa.charAt(0).toUpperCase() +
+      lensa.slice(1).toLocaleLowerCase()
+  );
+
+  const [powerEdit, setPowerEdit] = useState({
+    id: "",
+    power_name: "",
+    inStock: 0,
+    stock: 0,
+  });
 
   const columns = [
     {
@@ -38,31 +51,75 @@ const StokLensa = () => {
         },
       ],
     },
+    {
+      name: "Action",
+      selector: (row) => (
+        <button
+          className="btn btn-xs btn-success"
+          data-toggle="modal"
+          data-target="#modal-edit"
+          onClick={() =>
+            setPowerEdit({
+              id: row.id,
+              power_name: row.power_name,
+              inStock: row.stock,
+              stock: row.stock,
+            })
+          }
+        >
+          Edit
+        </button>
+      ),
+      width: "20%",
+    },
   ];
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const URL = import.meta.env.VITE_API_URL;
-        const response = await axios.get(URL + "api/product/" + id, {
+  const getData = async () => {
+    try {
+      const response = await axios.get(URL + "api/product/" + id, {
+        headers: {
+          Authorization: localStorage.getItem("user-token"),
+        },
+      });
+      if (response.data.success) {
+        setData(response.data.data);
+        setFilter(response.data.data);
+        setLensa(response.data.product_name);
+      } else {
+        localStorage.clear();
+        return navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateStock = async () => {
+    try {
+      const response = await axios.put(
+        URL + "api/product/" + powerEdit.id,
+        { stock: powerEdit.stock },
+        {
           headers: {
             Authorization: localStorage.getItem("user-token"),
           },
-        });
-        if (response.data.success) {
-          setData(response.data.data);
-          setFilter(response.data.data);
-        } else {
-          localStorage.clear();
-          return navigate("/login");
         }
-      } catch (error) {
-        console.log(error);
+      );
+      if (response.data.success) {
+        getData();
+        alert(response.data.message);
+      } else {
+        localStorage.clear();
+        return navigate("/login");
       }
-    };
+    } catch (error) {
+      throw error.message;
+    }
+  };
 
+  useEffect(() => {
     getData();
-  }, [id, navigate]);
+  }, []);
 
   useEffect(() => {
     const result = data.filter((item) => {
@@ -73,7 +130,7 @@ const StokLensa = () => {
 
   return (
     <div className="content-wrapper">
-      <Breadcrumb title="Data Lensa" />
+      <Breadcrumb title={lensa} />
       {/* Main Content */}
       <div className="content">
         <div className="container-fluid">
@@ -109,6 +166,85 @@ const StokLensa = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* Modal edit */}
+      <div
+        className="modal fade"
+        id="modal-edit"
+        data-keyboard="false"
+        data-backdrop="static"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit {lensa}</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <label htmlFor="">{powerEdit.power_name}</label>
+              <div className="row">
+                <div className="col p-0">
+                  <label htmlFor="">In Stock</label>
+                  <input
+                    type="text"
+                    className="form-control rounded-0"
+                    value={powerEdit.inStock}
+                    onChange={(e) =>
+                      setPowerEdit((powerEdit) => ({
+                        ...powerEdit,
+                        inStock: e.target.value,
+                      }))
+                    }
+                    disabled
+                  />
+                </div>
+                <div className="col p-0">
+                  <label htmlFor="">Actual Stock</label>
+                  <input
+                    type="text"
+                    className="form-control rounded-0"
+                    value={powerEdit.stock}
+                    onChange={(e) =>
+                      setPowerEdit((powerEdit) => ({
+                        ...powerEdit,
+                        stock: e.target.value,
+                      }))
+                    }
+                    autoFocus
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer justify-content-between">
+              <button
+                type="button"
+                className="btn btn-default"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                onClick={() => {
+                  updateStock();
+                }}
+                type="button"
+                className="btn btn-primary"
+                data-dismiss="modal"
+              >
+                Simpan
+              </button>
+            </div>
+          </div>
+          {/* Modal Content */}
+        </div>
+        {/* Modal Dialog */}
       </div>
     </div>
   );
