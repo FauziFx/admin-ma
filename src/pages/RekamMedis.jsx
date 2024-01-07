9697497a3eea4f92f4c4c352aa61236be31b1fcd
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-01-07 11:48:46 +0700

    feat(rekammedis): modal data pasien & modal add rekam medis

--- src/pages/RekamMedis.jsx
+++ src/pages/RekamMedis.jsx
@@ -5,14 +5,45 @@ import DataTable from "react-data-table-component";
 import moment from "moment-timezone";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 const RekamMedis = () => {
   useDocumentTitle("Data Rekam Medis");
   const URL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
+  const closeModal = useRef(null);
+  // Rekam medis
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState([]);
+  // Data Pasien
+  const [dataPasien, setDataPasien] = useState([]);
+  const [searchPasien, setSearchPasien] = useState("");
+  const [filterPasien, setFilterPasien] = useState([]);
+  const [pasienId, setPasienId] = useState(0);
+
+  const [dataUkuranBaru, setDataUkuranBaru] = useState({
+    nama: "",
+    alamat: "",
+    ttl: "",
+  });
+
+  const [ukuranBaru, setUkuranBaru] = useState({
+    rsph: "",
+    rcyl: "",
+    raxis: "",
+    radd: "",
+    lsph: "",
+    lcyl: "",
+    laxis: "",
+    ladd: "",
+    pd_jauh: "",
+    pd_dekat: "",
+    tanggal_periksa: moment().locale("id").format("YYYY-MM-DD"),
+    pemeriksa: "",
+    keterangan: "",
+  });
+
   const [detail, setDetail] = useState({
     nama: "",
     alamat: "",
@@ -78,6 +109,69 @@ const RekamMedis = () => {
     },
   ];
 
+  const columnsPasien = [
+    {
+      name: "Nama",
+      selector: (row) => row.nama.toUpperCase(),
+    },
+    {
+      name: "TTL",
+      selector: (row) => row.ttl,
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+        <button
+          className="btn btn-xs btn-primary mr-1"
+          data-dismiss="modal"
+          aria-label="Close"
+          data-toggle="modal"
+          data-target="#modal-ukuran-baru"
+          onClick={() => {
+            setPasienId(row.id);
+            setDataUkuranBaru({
+              nama: row.nama,
+              alamat: row.alamat,
+              ttl: row.ttl,
+            });
+          }}
+        >
+          Pilih
+        </button>
+      ),
+      width: "fit-content",
+    },
+  ];
+
+  const handleChangeUkuranBaru = async (e) => {
+    setUkuranBaru((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleClose = async () => {
+    closeModal.current.click();
+
+    setTimeout(() => {
+      setUkuranBaru({
+        rsph: "",
+        rcyl: "",
+        raxis: "",
+        radd: "",
+        lsph: "",
+        lcyl: "",
+        laxis: "",
+        ladd: "",
+        pd_jauh: "",
+        pd_dekat: "",
+        tanggal_periksa: moment().locale("id").format("YYYY-MM-DD"),
+        pemeriksa: "",
+        keterangan: "",
+      });
+    }, 2000);
+  };
+
   const showDetail = async (row) => {
     setDetail({
       nama: row.nama.toUpperCase(),
@@ -100,6 +194,58 @@ const RekamMedis = () => {
     });
   };
 
+  const submitUkuranbaru = async (e) => {
+    e.preventDefault();
+    try {
+      const od = [
+        ukuranBaru.rsph,
+        ukuranBaru.rcyl,
+        ukuranBaru.raxis,
+        ukuranBaru.radd,
+      ].join("/");
+      const os = [
+        ukuranBaru.lsph,
+        ukuranBaru.lcyl,
+        ukuranBaru.laxis,
+        ukuranBaru.ladd,
+      ].join("/");
+
+      const data = {
+        od: od,
+        os: os,
+        pd_jauh: parseInt(ukuranBaru.pd_jauh),
+        pd_dekat: parseInt(ukuranBaru.pd_dekat),
+        tanggal_periksa: ukuranBaru.tanggal_periksa,
+        pemeriksa: ukuranBaru.pemeriksa,
+        keterangan: ukuranBaru.keterangan,
+        pasien_id: pasienId,
+      };
+
+      const response = await axios.post(
+        URL + "api/rekam",
+        {
+          od: od,
+          os: os,
+          pd_jauh: parseInt(ukuranBaru.pd_jauh),
+          pd_dekat: parseInt(ukuranBaru.pd_dekat),
+          tanggal_periksa: ukuranBaru.tanggal_periksa,
+          pemeriksa: ukuranBaru.pemeriksa,
+          keterangan: ukuranBaru.keterangan,
+          pasien_id: pasienId,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("user-token"),
+          },
+        }
+      );
+      alert("Data Berhasil Disimpan...!");
+      await getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getData = async () => {
     try {
       const response = await axios.get(URL + "api/rekam", {
@@ -120,16 +266,46 @@ const RekamMedis = () => {
     }
   };
 
+  const getDataPasien = async () => {
+    try {
+      const response = await axios.get(URL + "api/pasien", {
+        headers: {
+          Authorization: localStorage.getItem("user-token"),
+        },
+      });
+
+      if (response.data.success) {
+        setDataPasien(response.data.data);
+        setFilterPasien(response.data.data);
+      } else {
+        localStorage.clear();
+        return navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getData();
+    getDataPasien();
   }, []);
 
+  // Rekam Medis
   useEffect(() => {
     const result = data.filter((item) => {
       return item.nama.toLowerCase().includes(search.toLocaleLowerCase());
     });
     setFilter(result);
   }, [data, search]);
+
+  // Data Pasien
+  useEffect(() => {
+    const result = dataPasien.filter((item) => {
+      return item.nama.toLowerCase().includes(searchPasien.toLocaleLowerCase());
+    });
+    setFilterPasien(result);
+  }, [dataPasien, searchPasien]);
 
   return (
     <div className="content-wrapper">
@@ -141,6 +317,13 @@ const RekamMedis = () => {
             <div className="col-lg-12">
               <div className="card">
                 <div className="card-header">
+                  <button
+                    className="btn btn-sm btn-primary mb-1"
+                    data-toggle="modal"
+                    data-target="#modal-pilih-pasien"
+                  >
+                    <i className="fas fa-plus"></i> Tambah Data
+                  </button>
                   <input
                     type="text"
                     className="form-control"
@@ -186,7 +369,7 @@ const RekamMedis = () => {
                 <span>x</span>
               </button>
             </div>
-            <div className="modal-body">
+            <div className="modal-body modal-body-overflow">
               <table id="table-detail" className="table table-sm">
                 <tbody>
                   <tr>
@@ -307,6 +490,294 @@ const RekamMedis = () => {
                 Close
               </button>
             </div>
+          </div>
+          {/* Modal Content */}
+        </div>
+        {/* Modal Dialog */}
+      </div>
+
+      {/* Modal Pilih Pasien */}
+      <div
+        className="modal fade"
+        id="modal-pilih-pasien"
+        data-keyboard="false"
+        data-backdrop="static"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Pilih Pasien</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span>x</span>
+              </button>
+            </div>
+            <div className="modal-body modal-body-overflow">
+              <input
+                type="text"
+                className="form-control form-control"
+                placeholder="Cari Namanya Disini..."
+                value={searchPasien}
+                onChange={(e) => setSearchPasien(e.target.value)}
+                autoFocus
+              />
+              <DataTable
+                columns={columnsPasien}
+                data={filterPasien}
+                pagination
+                highlightOnHover
+                customStyles={tableCustomStyles}
+              />
+            </div>
+            <div className="modal-footer justify-content-between">
+              <button
+                type="button"
+                className="btn btn-default"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+          {/* Modal Content */}
+        </div>
+        {/* Modal Dialog */}
+      </div>
+
+      {/* Modal Ukuran Baru */}
+      <div
+        className="modal fade"
+        id="modal-ukuran-baru"
+        data-keyboard="false"
+        data-backdrop="static"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Ukuran Kacamata Baru</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                ref={closeModal}
+              ></button>
+            </div>
+            <form onSubmit={submitUkuranbaru}>
+              <div className="modal-body">
+                <table className="table table-sm">
+                  <tbody>
+                    <tr>
+                      <td>Nama</td>
+                      <td>:</td>
+                      <td>{dataUkuranBaru.nama}</td>
+                    </tr>
+                    <tr>
+                      <td>Alamat</td>
+                      <td>:</td>
+                      <td>{dataUkuranBaru.alamat}</td>
+                    </tr>
+                    <tr>
+                      <td>TTL</td>
+                      <td>:</td>
+                      <td>{dataUkuranBaru.ttl}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="row">
+                  <div className="col-1 pt-1 text-bold">OD</div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.rsph}
+                      type="text"
+                      className="form-control"
+                      placeholder="SPH"
+                      name="rsph"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.rcyl}
+                      type="text"
+                      className="form-control"
+                      placeholder="CYL"
+                      name="rcyl"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.raxis}
+                      type="text"
+                      className="form-control"
+                      placeholder="AXIS"
+                      name="raxis"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.radd}
+                      type="text"
+                      className="form-control"
+                      placeholder="ADD"
+                      name="radd"
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-1 pt-1 text-bold">OS</div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.lsph}
+                      type="text"
+                      className="form-control"
+                      placeholder="SPH"
+                      name="lsph"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.lcyl}
+                      type="text"
+                      className="form-control"
+                      placeholder="CYL"
+                      name="lcyl"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.laxis}
+                      type="text"
+                      className="form-control"
+                      placeholder="AXIS"
+                      name="laxis"
+                    />
+                  </div>
+                  <div className="col p-0">
+                    <input
+                      onChange={(e) => handleChangeUkuranBaru(e)}
+                      value={ukuranBaru.ladd}
+                      type="text"
+                      className="form-control"
+                      placeholder="ADD"
+                      name="ladd"
+                    />
+                  </div>
+                </div>
+                <div className="form-group mb-1">
+                  <label htmlFor="" className="mb-0">
+                    PD :
+                  </label>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <input
+                          onChange={(e) => handleChangeUkuranBaru(e)}
+                          value={ukuranBaru.pd_jauh}
+                          type="text"
+                          className="form-control"
+                          placeholder="PD Jauh"
+                          name="pd_jauh"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <input
+                          onChange={(e) => handleChangeUkuranBaru(e)}
+                          value={ukuranBaru.pd_dekat}
+                          type="text"
+                          className="form-control"
+                          placeholder="PD Dekat"
+                          name="pd_dekat"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group mb-1">
+                  <label htmlFor="" className=" mb-0">
+                    Tanggal Periksa :
+                  </label>
+                  <input
+                    onChange={(e) => handleChangeUkuranBaru(e)}
+                    value={ukuranBaru.tanggal_periksa}
+                    type="date"
+                    name="tanggal_periksa"
+                    id=""
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group mb-1">
+                  <label htmlFor="" className="mb-0">
+                    Pemeriksa :
+                  </label>
+                  <input
+                    onChange={(e) => handleChangeUkuranBaru(e)}
+                    value={ukuranBaru.pemeriksa}
+                    type="text"
+                    name="pemeriksa"
+                    id=""
+                    className="form-control"
+                    placeholder="Pemeriksa"
+                    required
+                  />
+                </div>
+                <div className="form-group mb-0">
+                  <label htmlFor="" className=" mb-0">
+                    Keterangan :
+                  </label>
+                  <textarea
+                    onChange={(e) => handleChangeUkuranBaru(e)}
+                    value={ukuranBaru.keterangan}
+                    name="keterangan"
+                    className="form-control"
+                    id=""
+                    cols="30"
+                    rows="2"
+                    placeholder="Keluhan / Keterangan lain"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="modal-footer justify-content-between">
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  data-dismiss="modal"
+                  data-toggle="modal"
+                  data-target="#modal-pilih-pasien"
+                >
+                  Back
+                </button>
+                <div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary ml-1"
+                    onClick={() => handleClose()}
+                    disabled={
+                      ukuranBaru.rsph.length === 0 ||
+                      ukuranBaru.lsph.length === 0 ||
+                      ukuranBaru.pemeriksa.length === 0
+                    }
+                  >
+                    Simpan
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
           {/* Modal Content */}
         </div>
