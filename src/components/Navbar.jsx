521701cffddceba9f53d2acdba1230a9d742f01b
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-01-09 06:14:00 +0700

    style: add left navbar push menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,14 @@ const Navbar = () => {
   return (
     <nav className="main-header navbar navbar-expand navbar-white navbar-light">
       {/* Left navbar links */}
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" data-widget="pushmenu" href="#" role="button">
+            <i className="fas fa-bars" />
+          </a>
+        </li>
+      </ul>
+
       <ul className="navbar-nav ml-auto">
         {/* Messages Dropdown Menu */}
         <li className="nav-item dropdown">
