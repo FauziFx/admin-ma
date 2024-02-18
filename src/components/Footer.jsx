6d08a4eb0ba8d6d9f6c1faeb2f82b73d3aa67b7e
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-02-18 20:52:16 +0700

    style: edit footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="main-footer">
-      <strong>Copyright © 2014 Admin MA </strong>
+      <strong>Copyright © {year} Admin MA </strong>
       All rights reserved.
       <div className="float-right d-none d-sm-inline-block">
         <b>
-          Dibuat dengan <i className="fas fa-coffee"></i>
+          Dibuat dengan <i className="fas fa-coffee fa-xs"></i>
         </b>
-        &nbsp; Di Rumah
+        &nbsp; Di Rumah & Bahagia
       </div>
     </footer>
   );
