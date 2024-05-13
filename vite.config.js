c5b3a1c21eaeaf41b77984be31c481336736d640
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-05-14 05:26:12 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://fauziits.my.id/",
+  base: "https://cekstok.murtiaji.com/",
   build: { chunkSizeWarningLimit: 1600 },
 });
